eac4258aa32b5dd3deb9ea5eca32f53d38d8ffd4
marioalbornoz <mario.albornozc@utem.cl>
2020-06-10 12:52:14 -0400

    prod

--- folioe/settings/production.py
+++ folioe/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://folioutem.herokuapp.com/']
+ALLOWED_HOSTS = ['folioutem.herokuapp.com']
 
 
 # Database
