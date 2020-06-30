9ce0181e95a94f774d044a71e0de23577ce75f49
marioalbornoz <mario.albornozc@utem.cl>
2020-06-30 19:12:55 -0400

    actualizacion para produccion

--- folioe/settings/base.py
+++ folioe/settings/base.py
@@ -95,8 +95,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 LOGIN_REDIRECT_URL = reverse_lazy('curriculum_module:home')
