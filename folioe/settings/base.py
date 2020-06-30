ff624bb4c6220f32b9f4f810be9c7a997012ab98
marioalbornoz <mario.albornozc@utem.cl>
2020-06-30 19:45:28 -0400

    whitenoise

--- folioe/settings/base.py
+++ folioe/settings/base.py
@@ -37,8 +37,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = 'folioe.urls'
 
 TEMPLATES = [
