e035994eab6e2f8bf65c97ceb843d9d0dfd31bfd
marioalbornoz <mario.albornozc@utem.cl>
2020-06-30 18:58:20 -0400

    se agrega conf static

--- folioe/settings/base.py
+++ folioe/settings/base.py
@@ -96,6 +96,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 LOGIN_REDIRECT_URL = reverse_lazy('curriculum_module:home')
