72b3bfccf2966c2f30cc1710437a55de578d4452
marioalbornoz <mario.albornozc@utem.cl>
2020-06-30 20:52:42 -0400

    host nuevo

--- folioe/settings/production.py
+++ folioe/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['folioutem.herokuapp.com']
+ALLOWED_HOSTS = ['folioestudiantil.herokuapp.com']
 
 
 # Database
